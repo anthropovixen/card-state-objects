a3a2d365c632525cc44ad92ac692a924fa7c894a
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-03-03 13:08:39 +1100

    Create app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import userimage from "./images/user.png";
+import starimage from "./images/star-empty.png";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [contact, setContact] = React.useState({
+    firstName: "John",
+    lastName: "Doe",
+    phone: "+1 (719) 555-1212",
+    email: "itsmyrealname@example.com",
+    isFavorite: false,
+  });
+  /**
+   * Challenge: Fill in the values in the markup
+   * using the properties of our state object above
+   * (Ignore `isFavorite` for now)
+   */
+
+  function toggleFavorite() {
+    console.log("Toggle Favorite");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <article className="card">
+        <img src={userimage} className="card--image" alt="user" />
+        <div className="card--info">
+          <img
+            src={starimage}
+            className="card--favorite"
+            onClick={toggleFavorite}
+            alt="star"
+          />
+          <h2 className="card--name">
+            {contact.firstName} {contact.lastName}
+          </h2>
+          <p className="card--contact">{contact.phone}</p>
+          <p className="card--contact">{contact.email}</p>
+        </div>
+      </article>
+    </main>
   );
 }
-
-export default App;
